04206802980102a978a63fc2accfed1b69ea21bf
Thibaut Durand <durand.tibo@gmail.com>
2023-01-06 19:46:29 -0800

    Add PyTorch resolvers

--- tests/unit/test_pytorch.py
+++ tests/unit/test_pytorch.py
@@ -1,8 +1,12 @@
-import torch
 from omegaconf import OmegaConf
-from pytest import fixture
+from pytest import fixture, mark
 
-from bearface import register_resolvers
+from bearface import is_torch_available, register_resolvers
+
+if is_torch_available():
+    import torch
+
+torch_available = mark.skipif(not is_torch_available(), reason="Requires PyTorch")
 
 
 @fixture(scope="module", autouse=True)
@@ -10,9 +14,11 @@ def register() -> None:
     register_resolvers()
 
 
+@torch_available
 def test_to_tensor_resolver_number():
     assert OmegaConf.create({"key": "${bf.to_tensor:1.42}"}).key.equal(torch.tensor(1.42))
 
 
+@torch_available
 def test_to_tensor_resolver_list():
     assert OmegaConf.create({"key": "${bf.to_tensor:[1, 2, 3]}"}).key.equal(torch.tensor([1, 2, 3]))

7a464fe133dd5237b123527f569ad1ccea7c4052
Thibaut Durand <durand.tibo@gmail.com>
2023-01-06 21:13:45 -0800

    Clean and improve resolvers

--- tests/unit/test_resolvers.py
+++ tests/unit/test_resolvers.py
@@ -9,8 +9,28 @@ def register() -> None:
     register_resolvers()
 
 
-def test_add_resolver():
+def test_add_resolver_int2():
     assert OmegaConf.create({"key": "${bf.add:1,4}"}).key == 5
+
+
+def test_add_resolver_int3():
+    assert OmegaConf.create({"key": "${bf.add:1,2,4}"}).key == 7
+
+
+def test_add_resolver_list2():
+    assert OmegaConf.create({"key": "${bf.add:[1,2,3],[4]}"}).key == [1, 2, 3, 4]
+
+
+def test_add_resolver_list3():
+    assert OmegaConf.create({"key": "${bf.add:[1,2,3],[4],[5,6]}"}).key == [1, 2, 3, 4, 5, 6]
+
+
+def test_add_resolver_str2():
+    assert OmegaConf.create({"key": "${bf.add:abc,d}"}).key == "abcd"
+
+
+def test_add_resolver_str3():
+    assert OmegaConf.create({"key": "${bf.add:abc,d,ef}"}).key == "abcdef"
 
 
 def test_floordiv_resolver():
@@ -21,41 +41,24 @@ def test_neg_resolver():
     assert OmegaConf.create({"key": "${bf.neg:2}"}).key == -2
 
 
-def test_mergelist_resolver_1():
-    assert OmegaConf.create({"key": "${bf.mergelist:[1,2,3]}"}).key == [1, 2, 3]
-
-
-def test_mergelist_resolver_2():
-    assert OmegaConf.create({"key": "${bf.mergelist:[1,2,3],[2,3,4]}"}).key == [
-        1,
-        2,
-        3,
-        2,
-        3,
-        4,
-    ]
-
-
-def test_mergelist_resolver_3():
-    assert OmegaConf.create({"key": "${bf.mergelist:[1,2,3],[2,3,4],[a,b,c]}"}).key == [
-        1,
-        2,
-        3,
-        2,
-        3,
-        4,
-        "a",
-        "b",
-        "c",
-    ]
-
-
-def test_mul_resolver_int():
+def test_mul_resolver_int2():
     assert OmegaConf.create({"key": "${bf.mul:3,4}"}).key == 12
 
 
-def test_mul_resolver_float():
+def test_mul_resolver_int3():
+    assert OmegaConf.create({"key": "${bf.mul:3,4,2}"}).key == 24
+
+
+def test_mul_resolver_float2():
     assert OmegaConf.create({"key": "${bf.mul:1.5,3}"}).key == 4.5
+
+
+def test_mul_resolver_float3():
+    assert OmegaConf.create({"key": "${bf.mul:1.5,3,2.0}"}).key == 9.0
+
+
+def test_mul_resolver_list():
+    assert OmegaConf.create({"key": "${bf.mul:[1,2,3],3}"}).key == [1, 2, 3, 1, 2, 3, 1, 2, 3]
 
 
 def test_pow_resolver_int():
@@ -64,24 +67,6 @@ def test_pow_resolver_int():
 
 def test_pow_resolver_float():
     assert OmegaConf.create({"key": "${bf.pow:2.5,3}"}).key == 15.625
-
-
-def test_repeatlist_resolver_list():
-    assert OmegaConf.create({"key": "${bf.repeatlist:[1,2,3],3}"}).key == [
-        1,
-        2,
-        3,
-        1,
-        2,
-        3,
-        1,
-        2,
-        3,
-    ]
-
-
-def test_repeatlist_resolver_str():
-    assert OmegaConf.create({"key": "${bf.repeatlist:abc,2}"}).key == ["abc", "abc"]
 
 
 def test_sqrt_resolver():

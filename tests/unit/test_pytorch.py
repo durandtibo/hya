dd60053fe01358bfa14e34e046b44b8301c139b3
Thibaut Durand <durand.tibo@gmail.com>
2023-03-18 21:44:40 -0700

    Update CI workflow (#37)

--- tests/unit/test_pytorch.py
+++ tests/unit/test_pytorch.py
@@ -1,11 +1,15 @@
 from omegaconf import OmegaConf
-from pytest import fixture, mark
+from omegaconf.errors import InterpolationResolutionError
+from pytest import fixture, mark, raises
 
 from hya import is_torch_available, register_resolvers
-from hya.pytorch import DTYPE_MAPPING
+from hya.pytorch import get_dtypes
 
 if is_torch_available():
     import torch
+    from torch import dtype
+else:
+    dtype = None
 
 torch_available = mark.skipif(not is_torch_available(), reason="Requires PyTorch")
 
@@ -28,6 +32,35 @@ def test_to_tensor_resolver_list():
 
 
 @torch_available
-@mark.parametrize("target,dtype", DTYPE_MAPPING.items())
-def test_torch_dtype_resolver_list(target: str, dtype: torch.dtype):
-    assert OmegaConf.create({"key": "${hya.torch_dtype:" + f"{target}" + "}"}).key == dtype
+def test_torch_dtype_resolver_float():
+    assert OmegaConf.create({"key": "${hya.torch_dtype:float}"}).key == torch.float
+
+
+@torch_available
+def test_torch_dtype_resolver_long():
+    assert OmegaConf.create({"key": "${hya.torch_dtype:long}"}).key == torch.long
+
+
+@torch_available
+def test_torch_dtype_resolver_bool():
+    assert OmegaConf.create({"key": "${hya.torch_dtype:bool}"}).key == torch.bool
+
+
+@torch_available
+def test_torch_dtype_resolver_incorrect_attribute():
+    with raises(InterpolationResolutionError):
+        OmegaConf.create({"key": "${hya.torch_dtype:bool32}"}).key
+
+
+@torch_available
+def test_torch_dtype_resolver_incorrect_dtype():
+    with raises(InterpolationResolutionError):
+        OmegaConf.create({"key": "${hya.torch_dtype:ones}"}).key
+
+
+@torch_available
+def test_get_dtypes():
+    dtypes = get_dtypes()
+    assert len(dtypes) > 1
+    assert torch.float in dtypes
+    assert torch.long in dtypes

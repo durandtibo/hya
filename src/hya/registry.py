94a03490a8c9db0d9f06fb26700370c3fa7426fc
Thibaut Durand <durand.tibo@gmail.com>
2023-05-20 08:59:17 -0700

    Renaming

--- src/hya/registry.py
+++ src/hya/registry.py
@@ -13,7 +13,7 @@ class ResolverRegistry:
 
     @property
     def state(self) -> dict[str, Callable]:
-        r"""Dict: The state of the registry."""
+        r"""``dict``: The state of the registry."""
         return self._state
 
     def register(self, key: str, exist_ok: bool = False) -> Callable:

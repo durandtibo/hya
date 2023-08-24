3825a1b40e106ca1246946d0f389f3cce061c5ab
Thibaut Durand <durand.tibo@gmail.com>
2023-08-24 13:43:27 -0700

    Improve resolvers and registry (#96)

--- tests/unit/test_imports.py
+++ tests/unit/test_imports.py
@@ -1,6 +1,21 @@
 from __future__ import annotations
 
-from hya.imports import is_torch_available
+from unittest.mock import patch
+
+from pytest import raises
+
+from hya.imports import check_torch, is_torch_available
+
+
+def test_check_torch_with_package() -> None:
+    with patch("hya.imports.is_torch_available", lambda *args: True):
+        check_torch()
+
+
+def test_check_torch_without_package() -> None:
+    with patch("hya.imports.is_torch_available", lambda *args: False):
+        with raises(RuntimeError, match="`torch` package is required but not installed."):
+            check_torch()
 
 
 def test_is_torch_available() -> None:

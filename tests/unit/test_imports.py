64baa22091b0104b57190a4f4490adb250bddfdd
Thibaut Durand <durand.tibo@gmail.com>
2023-09-15 17:32:47 -0700

    Add optional braceexpand dependency (#109)

--- tests/unit/test_imports.py
+++ tests/unit/test_imports.py
@@ -4,7 +4,36 @@ from unittest.mock import patch
 
 from pytest import raises
 
-from hya.imports import check_torch, is_torch_available
+from hya.imports import (
+    check_braceexpand,
+    check_torch,
+    is_braceexpand_available,
+    is_torch_available,
+)
+
+#######################
+#     braceexpand     #
+#######################
+
+
+def test_check_braceexpand_with_package() -> None:
+    with patch("hya.imports.is_braceexpand_available", lambda *args: True):
+        check_braceexpand()
+
+
+def test_check_braceexpand_without_package() -> None:
+    with patch("hya.imports.is_braceexpand_available", lambda *args: False):
+        with raises(RuntimeError, match="`braceexpand` package is required but not installed."):
+            check_braceexpand()
+
+
+def test_is_braceexpand_available() -> None:
+    assert isinstance(is_braceexpand_available(), bool)
+
+
+#################
+#     torch     #
+#################
 
 
 def test_check_torch_with_package() -> None:

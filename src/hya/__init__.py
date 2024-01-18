fcb389cfe403812944d22fc2da480ec4eb5043e2
Thibaut Durand <durand.tibo@gmail.com>
2024-01-17 17:01:11 -0800

    Update numpy array resolver

--- src/hya/__init__.py
+++ src/hya/__init__.py
@@ -10,10 +10,12 @@ __all__ = [
 ]
 
 from hya import resolvers  # noqa: F401
-from hya.imports import is_braceexpand_available, is_torch_available
+from hya.imports import is_braceexpand_available, is_numpy_available, is_torch_available
 from hya.registry import register_resolvers
 
 if is_braceexpand_available():  # pragma: no cover
     from hya import braceexpand_  # noqa: F401
+if is_numpy_available():  # pragma: no cover
+    from hya import numpy_  # noqa: F401
 if is_torch_available():  # pragma: no cover
     from hya import torch_  # noqa: F401
